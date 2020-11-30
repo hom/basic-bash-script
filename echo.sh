657c42a6788cc1c2aad5093d6053409ae70849fe
mengj <813935532@qq.com>
2020-11-30 11:29:25 +0800

    feat: modify script variable

--- echo.sh
+++ echo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "The time is `date` with \` wrapped"
-echo "The time is $(date) with \$\(\) wrapped"
+echo "The time is $(date) with \$() wrapped"
 
 now=$(date)
 echo "The variable value is $now with only \$"
-echo "The variable value is ${now} with \$\{\}"
+echo "The variable value is ${now} with \${}"
