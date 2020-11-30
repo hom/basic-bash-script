67ac4b3f9c3d1eaf68bb123e4f157082215a12c0
mengj <813935532@qq.com>
2020-11-30 14:40:00 +0800

    feat: modify for loop

--- for.sh
+++ for.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+# Echo list with space deviced
 users='mengj guom tj'
-
-for user in ${users}
-do
+for user in ${users}; do
   echo ${user}
+done
+
+# Echo list with , deviced
+list='mengj,guom,tj'
+IFS=,
+for item in ${list}; do
+  echo ${item}
+done
+
+# Echo range numbers
+for num in {1..10}; do
+  echo ${num}
+done
+
+# Echo array
+array=('mengj' 'guom' 'tj')
+for item in ${array[@]}; do
+  echo ${item}
+done
+
+for item in ${array[*]}; do
+  echo ${item}
 done
