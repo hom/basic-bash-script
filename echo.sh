a578b578d0149b061f5c53fa035e46aca51e88fd
mengj <813935532@qq.com>
2020-11-30 11:27:23 +0800

    feat: add array and echo script

---
+++ echo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "The time is `date` with \` wrapped"
+echo "The time is $(date) with \$\(\) wrapped"
+
+now=$(date)
+echo "The variable value is $now with only \$"
+echo "The variable value is ${now} with \$\{\}"
